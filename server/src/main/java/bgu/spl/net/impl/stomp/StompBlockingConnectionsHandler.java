b1d665ed98c27724ee06c2a9c56e88fb181e18be
Tomer Ben Bassat <benbasst@post.bgu.ac.il>
2026-01-17 11:23:28 +0200

    balbalot

---
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompBlockingConnectionsHandler.java
@@ -0,0 +1,72 @@
+
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.api.MessagingProtocol;
+import bgu.spl.net.srv.ConnectionHandler;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+
+
+
+public class StompBlockingConnectionsHandler implements Runnable, ConnectionHandler<T> {
+
+    private final MessagingProtocol<T> protocol;
+    private final MessageEncoderDecoder<T> encdec;
+    private final Socket sock;
+    private BufferedInputStream in;
+    private BufferedOutputStream out;
+    private volatile boolean connected = true;
+
+    public BlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, MessagingProtocol<T> protocol) {
+        this.sock = sock;
+        this.encdec = reader;
+        this.protocol = protocol;
+    }
+
+    @Override
+    public void run() {
+        try (Socket sock = this.sock) { //just for automatic closing
+            int read;
+
+            in = new BufferedInputStream(sock.getInputStream());
+            out = new BufferedOutputStream(sock.getOutputStream());
+
+            while (!protocol.shouldTerminate() && connected && (read = in.read()) >= 0) {
+                T nextMessage = encdec.decodeNextByte((byte) read);
+                if (nextMessage != null) {
+                    protocol.process(nextMessage);
+                }
+                if (protocol.shouldTerminate()) {
+                    connected = false;
+                }
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void close() throws IOException {
+        connected = false;
+        sock.close();
+    }
+
+    @Override
+    public synchronized void send(T msg) { // Added synchronized
+        try { 
+            if (msg != null) {
+                out.write(encdec.encode(msg));
+                out.flush();
+            }
+        } catch (IOException e) {
+            // If writing fails, the connection is likely dead
+            connected = false; 
+        }
+    }
+}
